51a2d6b1673376e67994803b541e6e420a4dd0b8
AndyBeable <andy.beable@gmail.com>
2021-03-31 16:49:08 +0100

    fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
+import Tours from './Tours';
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
   if (loading) {
     return (
@@ -14,7 +33,11 @@ function App() {
     );
   }
 
-  return <h1>Tours Project Setup</h1>;
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
